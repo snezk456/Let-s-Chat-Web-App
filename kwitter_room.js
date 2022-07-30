ab56e39c42082422d4226fe01aaf6b0edeb1edad
snezk456 <86143353+snezk456@users.noreply.github.com>
2022-07-30 15:32:07 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -25,7 +25,7 @@ function addRoom()
 
     localStorage.setItem("room_name", room_name);
     
-    window.location = "chatApp_page.html";
+    window.location = "kwitter_page.html";
 }
 
 function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
@@ -44,5 +44,11 @@ function redirectToRoomName(name)
 {
   console.log(name);
   localStorage.setItem("room_name", name);
-    window.location = "chatApp_page.html";
+    window.location = "kwitter_page.html";
+}
+
+function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location = "index.html";
 }
